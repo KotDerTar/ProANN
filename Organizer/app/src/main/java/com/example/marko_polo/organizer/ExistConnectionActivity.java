ddcd15441b7c2b66004fffca4d6f152f36d57116
NataliiaTaran <n.taran713@gmail.com>
2014-11-15 23:06:06 +0200

    added help button

---
+++ Organizer/app/src/main/java/com/example/marko_polo/organizer/ExistConnectionActivity.java
@@ -0,0 +1,49 @@
+package com.example.marko_polo.organizer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class ExistConnectionActivity extends Activity {
+
+    EditText nameTxt, linkTxt, emailTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exist_connection);
+
+        nameTxt = (EditText) findViewById(R.id.txtName);
+        linkTxt = (EditText) findViewById(R.id.txtLink);
+        emailTxt = (EditText) findViewById(R.id.txtEmail);
+        final Button makeExistConnectionBtn = (Button) findViewById(R.id.btnMakeExistConnection);
+
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                makeExistConnectionBtn.setEnabled(!nameTxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void goBackToMyActivity (View v) {
+        Intent intent = new Intent(this, MyActivity.class);
+        startActivity(intent);
+    }
+}
