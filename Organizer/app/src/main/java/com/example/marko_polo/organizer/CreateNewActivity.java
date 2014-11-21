51687d8b0fda638ab1e6f35c770bac0de04df02c
AnnaKotiash <Anisha2009@ukr.net>
2014-11-22 01:01:09 +0200

    Prt sc

--- Organizer/app/src/main/java/com/example/marko_polo/organizer/CreateNewActivity.java
+++ Organizer/app/src/main/java/com/example/marko_polo/organizer/CreateNewActivity.java
@@ -13,14 +13,58 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.graphics.Bitmap; import android.graphics.drawable.BitmapDrawable;
+
+
+
+
 
 /**
  * Created by Marko_Polo on 21.11.2014.
  */
 public class CreateNewActivity extends Activity {
+    LinearLayout L1;
+    ImageView image;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //MakeServer();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new);
+        L1 = (LinearLayout) findViewById(R.id.CreateNew);
+        Button but = (Button) findViewById(R.id.start);
+
     }
+    public void onClick(View v){
+        View v1 = L1.getRootView();
+        v1.setDrawingCacheEnabled(true);
+        Bitmap bm = v1.getDrawingCache();
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(bm);
+        image = (ImageView) findViewById(R.id.CreateNew);
+        image.setBackgroundDrawable(bitmapDrawable); }
+   /* public void MakeServer(){
+        try{
+            ServerSocket serversoket = new ServerSocket(5000);
+            while(true){
+                Socket sock = serversoket.accept();
+                PrintWriter printwriter = new PrintWriter(sock.getOutputStream());
+                printwriter.println("Hello Kitti");
+                printwriter.close();
+            }
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+
+    }*/
 }
+
+
+
+
+
+
--- Organizer/app/src/main/java/com/example/marko_polo/organizer/MyActivity.java
+++ Organizer/app/src/main/java/com/example/marko_polo/organizer/MyActivity.java
@@ -2,10 +2,16 @@ package com.example.marko_polo.organizer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import java.io.*;
 import java.net.*;
 
@@ -18,11 +24,14 @@ import java.net.Socket;
 
 
 public class MyActivity extends Activity {
-
+    LinearLayout L1;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        L1 = (LinearLayout) findViewById(R.id.CreateNew);
+        Button but = (Button) findViewById(R.id.start);
     }
 
 
@@ -49,24 +58,21 @@ public class MyActivity extends Activity {
         Intent intent = new Intent(this, ExistConnectionActivity.class);
         startActivity(intent);
     }
+    public void goToCreateNewActivity (View v) {
+        Intent intent = new Intent(this, CreateNewActivity.class);
+        startActivity(intent);
+    }
 
     public void goToHelpActivity (View v){
         Intent intent = new Intent(this, HelpActivity.class);
         startActivity(intent);
     }
-    public void MakeServer(){
-        try{
-            ServerSocket serversoket = new ServerSocket(5000);
-            while(true){
-                Socket sock = serversoket.accept();
-                PrintWriter printwriter = new PrintWriter(sock.getOutputStream());
-                printwriter.println("Hello Kitti");
-                printwriter.close();
-            }
-        }
-        catch(IOException ex){
-            ex.printStackTrace();
-        }
+   /* public void onClick(View v){
+        View v1 = L1.getRootView();
+        v1.setDrawingCacheEnabled(true);
+        Bitmap bm = v1.getDrawingCache();
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(bm);
+        image = (ImageView) findViewById(R.id.CreateNew);
+        image.setBackgroundDrawable(bitmapDrawable); }*/
 
-    }
 }
